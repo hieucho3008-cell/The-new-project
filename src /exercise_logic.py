85e8aea99e0318d1a340088bfb3abb7639f3e419
hieucho3008-cell <hieucho3008@gmail.com>
2026-06-15 15:51:45 +0700

    Update exercise_logic.py

--- src /exercise_logic.py
+++ src /exercise_logic.py
@@ -1,1 +1,27 @@
+class DexterityExercise:
 
+    def __init__(self):
+
+        self.state = "OPEN"
+
+        self.repetitions = 0
+
+    def update(self, finger_count):
+
+        if finger_count >= 4:
+
+            current = "OPEN"
+
+        elif finger_count <= 1:
+
+            current = "CLOSED"
+
+        else:
+
+            return
+
+        if self.state == "OPEN" and current == "CLOSED":
+
+            self.repetitions += 1
+
+        self.state = current
