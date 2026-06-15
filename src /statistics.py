2ddcaa661a613bcc5bf1bd69ff5d8bd5c37bbdfe
hieucho3008-cell <hieucho3008@gmail.com>
2026-06-15 15:52:12 +0700

    Update statistics.py

--- src /statistics.py
+++ src /statistics.py
@@ -1,1 +1,23 @@
+import time
 
+class Statistics:
+
+    def __init__(self):
+
+        self.start_time = time.time()
+
+    def elapsed_time(self):
+
+        return round(
+            time.time() - self.start_time,
+            2
+        )
+
+    def average_speed(self, reps):
+
+        t = self.elapsed_time()
+
+        if reps == 0:
+            return 0
+
+        return round(t / reps, 2)
