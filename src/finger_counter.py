b7489199482e759f48bdfea72f7973327c919f2c
hieucho3008-cell <hieucho3008@gmail.com>
2026-06-15 19:00:22 +0700

    Update finger_counter.py

--- src/finger_counter.py
+++ src/finger_counter.py
@@ -1,13 +1,29 @@
 def count_fingers(hand):
 
     lmList = hand["lmList"]
+    handType = hand["type"]
 
     tips = [4, 8, 12, 16, 20]
 
     count = 0
 
-    if lmList[4][0] > lmList[3][0]:
-        count += 1
+    # =====================
+    # THUMB
+    # =====================
+
+    if handType == "Right":
+
+        if lmList[4][0] > lmList[3][0]:
+            count += 1
+
+    else:  # Left
+
+        if lmList[4][0] < lmList[3][0]:
+            count += 1
+
+    # =====================
+    # OTHER 4 FINGERS
+    # =====================
 
     for tip in tips[1:]:
 
