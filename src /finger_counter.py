c0ab05611ada074d8d8604d567cbe8d8d7e30093
hieucho3008-cell <hieucho3008@gmail.com>
2026-06-15 15:51:19 +0700

    Update finger_counter.py

--- src /finger_counter.py
+++ src /finger_counter.py
@@ -1,1 +1,17 @@
+def count_fingers(hand):
 
+    lmList = hand["lmList"]
+
+    tips = [4, 8, 12, 16, 20]
+
+    count = 0
+
+    if lmList[4][0] > lmList[3][0]:
+        count += 1
+
+    for tip in tips[1:]:
+
+        if lmList[tip][1] < lmList[tip - 2][1]:
+            count += 1
+
+    return count
