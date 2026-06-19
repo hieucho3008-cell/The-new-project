96c0995f21ac4b59ddef3d4ceb55112a80ec5bab
hieucho3008-cell <hieucho3008@gmail.com>
2026-06-19 19:19:08 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -205,7 +205,6 @@ while True:
     # ==========================
     # BACKGROUND UI PANEL
     # ==========================
-    cv2.rectangle(img, (20, 20), (520, 700), (255, 255, 255), -1)
     cv2.putText(img, "HAND DEXTERITY ASSESSMENT", (35, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 2)
     
     cv2.putText(img, f"Left Hand : {left_count}", (35, 110), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (50, 50, 50), 2)
